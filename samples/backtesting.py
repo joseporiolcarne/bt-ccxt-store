2cdc86d785ff37f79ae89a20f1dfb64187dbb451
invstia <invstia@proton.me>
2024-10-26 09:49:51 +0200

    adapting to binance

--- samples/backtesting.py
+++ samples/backtesting.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import time
 from datetime import datetime
 
@@ -13,7 +18,7 @@ def main():
 
         def next(self, dt=None):
             dt = dt or self.datas[0].datetime.datetime(0)
-            print('%s closing price: %s' % (dt.isoformat(), self.datas[0].close[0]))
+            print("%s closing price: %s" % (dt.isoformat(), self.datas[0].close[0]))
             self.next_runs += 1
 
     cerebro = bt.Cerebro()
@@ -21,22 +26,28 @@ def main():
     cerebro.addstrategy(TestStrategy)
 
     # Add the feed
-    cerebro.adddata(CCXTFeed(exchange='binance',
-                             dataname='BNB/USDT',
-                             timeframe=bt.TimeFrame.Minutes,
-                             fromdate=datetime(2019, 1, 1, 0, 0),
-                             todate=datetime(2019, 1, 1, 0, 2),
-                             compression=1,
-                             ohlcv_limit=2,
-                             currency='BNB',
-                             retries=5,
-
-                             # 'apiKey' and 'secret' are skipped
-                             config={'enableRateLimit': True, 'nonce': lambda: str(int(time.time() * 1000))}))
+    cerebro.adddata(
+        CCXTFeed(
+            exchange="binance",
+            dataname="BNB/USDT",
+            timeframe=bt.TimeFrame.Minutes,
+            fromdate=datetime(2024, 1, 1, 0, 0),
+            todate=datetime(2024, 1, 1, 0, 2),
+            compression=1,
+            ohlcv_limit=2,
+            currency="BNB",
+            retries=5,
+            # 'apiKey' and 'secret' are skipped
+            config={
+                "enableRateLimit": True,
+                "nonce": lambda: str(int(time.time() * 1000)),
+            },
+        )
+    )
 
     # Run the strategy
     cerebro.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- samples/binance/buy-order.py
+++ samples/binance/buy-order.py
@@ -1,7 +1,12 @@
 import json
 import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 import time
 from datetime import datetime, timedelta
+import logging
+from ccxt import binance
 
 import backtrader as bt
 from backtrader import Order
@@ -16,6 +21,8 @@ class TestStrategy(bt.Strategy):
         self.bought = False
         # To keep track of pending orders and buy price/commission
         self.order = None
+        self.buy_time = None
+        self.sell_order = None
 
     def next(self):
 
@@ -23,22 +30,41 @@ class TestStrategy(bt.Strategy):
             # Buy
             # size x price should be >10 USDT at a minimum at Binance
             # make sure you use a price that is below the market price if you don't want to actually buy
-            self.order = self.buy(size=2.0, exectype=Order.Limit, price=5.4326)
+            self.order = self.buy(size=2, exectype=Order.Market)
             # And immediately cancel the buy order
-            self.cancel(self.order);
+            # self.cancel(self.order)
             self.bought = True
+            self.buy_time = datetime.now()
+            print(f"Bought at {self.data.close[0]} at {self.buy_time}")
+
+        # Check if 3 minutes have passed since the buy
+        if self.bought and (datetime.now() - self.buy_time >= timedelta(minutes=3)):
+            # Sell at market price
+            if self.sell_order is None:
+                self.sell_order = self.sell(size=0.1, exectype=Order.Market)
+                print(f"Sold at {self.data.close[0]} at {datetime.now()}")
 
         for data in self.datas:
-            print('{} - {} | O: {} H: {} L: {} C: {} V:{}'.format(data.datetime.datetime(),
-                                                                  data._name, data.open[0], data.high[0], data.low[0],
-                                                                  data.close[0], data.volume[0]))
+            print(
+                "{} - {} | O: {} H: {} L: {} C: {} V:{}".format(
+                    data.datetime.datetime(),
+                    data._name,
+                    data.open[0],
+                    data.high[0],
+                    data.low[0],
+                    data.close[0],
+                    data.volume[0],
+                )
+            )
 
     def notify_data(self, data, status, *args, **kwargs):
         dn = data._name
         dt = datetime.now()
-        msg = 'Data Status: {}, Order Status: {}'.format(data._getstatusname(status), status)
+        msg = "Data Status: {}, Order Status: {}".format(
+            data._getstatusname(status), status
+        )
         print(dt, dn, msg)
-        if data._getstatusname(status) == 'LIVE':
+        if data._getstatusname(status) == "LIVE":
             self.live_data = True
         else:
             self.live_data = False
@@ -46,8 +72,8 @@ class TestStrategy(bt.Strategy):
 
 # absolute dir the script is in
 script_dir = os.path.dirname(__file__)
-abs_file_path = os.path.join(script_dir, '../params.json')
-with open(abs_file_path, 'r') as f:
+abs_file_path = os.path.join(script_dir, "../params.json")
+with open(abs_file_path, "r") as f:
     params = json.load(f)
 
 cerebro = bt.Cerebro(quicknotify=True)
@@ -58,13 +84,48 @@ cerebro.broker.setcash(10.0)
 cerebro.addstrategy(TestStrategy)
 
 # Create our store
-config = {'apiKey': params["binance"]["apikey"],
-          'secret': params["binance"]["secret"],
-          'enableRateLimit': True,
-          'nonce': lambda: str(int(time.time() * 1000)),
-          }
+config = {
+    "apiKey": params["binance"]["apikey"],
+    "secret": params["binance"]["secret"],
+    "enableRateLimit": True,
+    "nonce": lambda: str(int(time.time() * 1000)),
+    "options": {
+        "defaultType": "future",  # spot, margin, future
+    },
+}
 
-store = CCXTStore(exchange='binance', currency='BNB', config=config, retries=5, debug=True)
+logging.basicConfig(level=logging.DEBUG)
+
+# exchange = binance(
+#     {
+#         "apiKey": params["binance"]["apikey"],
+#         "secret": params["binance"]["secret"],
+#         "enableRateLimit": True,
+#         "options": {
+#             "defaultType": "future",  # spot, margin, future
+#         },
+#     }
+# )
+
+# exchange.set_sandbox_mode(True)
+
+# exchange.load_markets()
+
+
+# try:
+#     balance = exchange.fetch_balance()
+#     print(balance)
+# except Exception as e:
+#     logging.error("Error fetching balance: %s", e)
+
+store = CCXTStore(
+    exchange="binance",
+    currency="BNB",
+    config=config,
+    retries=5,
+    debug=False,
+    sandbox=True,
+)
 
 # Get the broker and pass any kwargs if needed.
 # ----------------------------------------------
@@ -72,33 +133,34 @@ store = CCXTStore(exchange='binance', currency='BNB', config=config, retries=5, 
 # to the defaults. Case in point, Kraken vs Bitmex. NOTE: Broker mappings are not
 # required if the broker uses the same values as the defaults in CCXTBroker.
 broker_mapping = {
-    'order_types': {
-        bt.Order.Market: 'market',
-        bt.Order.Limit: 'limit',
-        bt.Order.Stop: 'stop-loss',  # stop-loss for kraken, stop for bitmex
-        bt.Order.StopLimit: 'stop limit'
+    "order_types": {
+        bt.Order.Market: "market",
+        bt.Order.Limit: "limit",
+        bt.Order.Stop: "stop-loss",  # stop-loss for kraken, stop for bitmex
+        bt.Order.StopLimit: "stop limit",
     },
-    'mappings': {
-        'closed_order': {
-            'key': 'status',
-            'value': 'closed'
-        },
-        'canceled_order': {
-            'key': 'status',
-            'value': 'canceled'
-        }
-    }
+    "mappings": {
+        "closed_order": {"key": "status", "value": "closed"},
+        "canceled_order": {"key": "status", "value": "canceled"},
+    },
 }
 
 broker = store.getbroker(broker_mapping=broker_mapping)
 cerebro.setbroker(broker)
 
+
 # Get our data
 # Drop newest will prevent us from loading partial data from incomplete candles
 hist_start_date = datetime.utcnow() - timedelta(minutes=50)
-data = store.getdata(dataname='BNB/USDT', name="BNBUSDT",
-                     timeframe=bt.TimeFrame.Minutes, fromdate=hist_start_date,
-                     compression=1, ohlcv_limit=50, drop_newest=True)  # , historical=True)
+data = store.getdata(
+    dataname="BNB/USDT",
+    name="BNBUSDT",
+    timeframe=bt.TimeFrame.Minutes,
+    fromdate=hist_start_date,
+    compression=1,
+    ohlcv_limit=50,
+    drop_newest=True,
+)  # , historical=True)
 
 # Add the feed
 cerebro.adddata(data)
